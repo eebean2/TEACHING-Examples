603684c697672da89035b6071e805e467ca541e9
Erik Bean <eebean2@me.com>
2019-04-26 12:12:57 -0400

    Update Teaching Example

--- Teaching Live File.playground/Contents.swift
+++ Teaching Live File.playground/Contents.swift
@@ -1,26 +1,9 @@
 import UIKit
 
 
-let tableview = UITableView()
+var i: Int?
 
-class CustomCell: UITableViewCell { }
-class CustomCellTwo: UITableViewCell { }
-
-let id1 = "I'm An ID"
-let id2 = "I'm An ID Also"
-
-if let cell = tableview.dequeueReusableCell(withIdentifier: id1) as? CustomCell {
-    // modifiy cell
-//    return cell
-} else if let cell = tableview.dequeueReusableCell(withIdentifier: id2) as? CustomCellTwo {
-    // modify cell
-//    return cell
-}
-
-
-
-
-
+print("\(String(describing: i))")
 
 
 
